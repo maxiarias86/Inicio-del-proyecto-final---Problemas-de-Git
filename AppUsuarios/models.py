178ccc5d978a45e16ea1eee63c64229dd432fc28
maxiarias <maxi@MacBook-Air-de-Maxi.local>
2023-04-22 16:09:24 -0300

    Segundo commit, sin grandes avances

--- AppUsuarios/models.py
+++ AppUsuarios/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    #avatar=models.ImageField()
+    username=models.CharField(max_length=30,unique=True)
+    descripcion=models.CharField(max_length=20000)
+    pagina=models.URLField()
+    mail=models.EmailField(unique=True)
+    password=models.CharField(max_length=15)
+
+'''
+class Mensaje(models.Model):
+    remitente=models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    destinatario=models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    contenido=models.CharField(max_length=1000)
+'''
